957417db4ca475afb411aae0962bc7be3e4b0e6a
bsvab <bnsvab@gmail.com>
2024-02-26 21:32:39 -0600

    progress

--- code/static/map1_injection_layer.js
+++ code/static/map1_injection_layer.js
@@ -1,22 +1,14 @@
-// Initialize the map
-var map = L.map('map').setView([31.9686, -99.9018], 6);
-
-// Add OpenStreetMap tiles to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
 // Function to update the sidebar with currently displayed injections
 function updateList(timeline) {
-    var displayed = timeline.getLayers();
-    var list = document.getElementById("displayed-list");
+    let displayed = timeline.getLayers();
+    let list = document.getElementById("displayed-list");
     list.innerHTML = ""; // Clear existing list
     
     displayed.forEach(function (featureLayer) {
-        var feature = featureLayer.feature;
-        var properties = feature.properties;
+        let feature = featureLayer.feature;
+        let properties = feature.properties;
     
-        var li = document.createElement("li");
+        let li = document.createElement("li");
         li.innerHTML = "<strong>API Number:</strong> " + properties["API Number"] +
                        "<br><strong>County:</strong> " + properties.County +
                        "<br><strong>Field Name:</strong> " + (properties["Field Name"] || "Unavailable") +
@@ -33,30 +25,30 @@ function updateList(timeline) {
 fetch('../data/injection_data_v2.geojson')
     .then(response => response.json())
     .then(data => {
-        var getInterval = function (feature) {
-            var startTime = new Date(feature.properties["Injection Date"]).getTime();
-            var endTime = new Date(feature.properties["Injection End Date"]).getTime();
+        let getInterval = function (feature) {
+            let startTime = new Date(feature.properties["Injection Date"]).getTime();
+            let endTime = new Date(feature.properties["Injection End Date"]).getTime();
             return { start: startTime, end: endTime };
         };
 
-        var timeline = L.timeline(data, {
+        let timeline = L.timeline(data, {
             getInterval: getInterval,
             pointToLayer: function (feature, latlng) {
-                var volume = feature.properties["Volume Injected (BBLs)"];
-                var radius = Math.max(3, Math.sqrt(volume) / 20); 
+                let volume = feature.properties["Volume Injected (BBLs)"];
+                let radius = Math.max(3, Math.sqrt(volume) / 20); 
                 return L.circleMarker(latlng, { radius: radius }).bindPopup(`Volume Injected: ${volume} BBLs`);
             }
         });
 
-        var timelineControl = L.timelineSliderControl({
+        let timelineControl = L.timelineSliderControl({
             formatOutput: function (date) {
                 return new Date(date).toDateString();
             }
         });
 
-        timelineControl.addTo(map);
+        timelineControl.addTo(map1);
         timelineControl.addTimelines(timeline);
-        timeline.addTo(map);
+        timeline.addTo(map1);
 
         timeline.on('change', function () {
             updateList(timeline);
