7ecb01b9c80b14429aa5f5abe8e70f162832523d
bsvab <bnsvab@gmail.com>
2024-02-28 22:15:18 -0600

    progress

--- code/template/static/map1_injection_layer.js
+++ code/template/static/map1_injection_layer.js
@@ -1,28 +1,5 @@
-// // Function to update the sidebar with currently displayed injections
-// function updateList(timeline) {
-//     let displayed = timeline.getLayers();
-//     let list = document.getElementById("displayed-list");
-//     list.innerHTML = ""; // Clear existing list
-    
-//     displayed.forEach(function (featureLayer) {
-//         let feature = featureLayer.feature;
-//         let properties = feature.properties;
-    
-//         let li = document.createElement("li");
-//         li.innerHTML = "<strong>API Number:</strong> " + properties["API Number"] +
-//                        "<br><strong>County:</strong> " + properties.County +
-//                        "<br><strong>Field Name:</strong> " + (properties["Field Name"] || "Unavailable") +
-//                        "<br><strong>Injection Date:</strong> " + properties["Injection Date"] +
-//                        "<br><strong>Injection End Date:</strong> " + properties["Injection End Date"] +
-//                        "<br><strong>Injection Layer:</strong> " + properties["Injection Layer"] +
-//                        "<br><strong>Volume Injected (BBLs):</strong> " + properties["Volume Injected (BBLs)"];
-        
-//         list.appendChild(li);
-//     });
-// }
-
 // Load GeoJSON data and create a timeline
-fetch('.../data/injection_data_v2.geojson')
+fetch('../../data/injection_data_v2.geojson')
     .then(response => response.json())
     .then(data => {
         let getInterval = function (feature) {
@@ -40,17 +17,17 @@ fetch('.../data/injection_data_v2.geojson')
             }
         });
 
-        let timelineControl = L.timelineSliderControl({
-            formatOutput: function (date) {
-                return new Date(date).toDateString();
-            }
-        });
+        // MOVED BELOW CODE TO MAP1.JS FILE - DO NOT UNCOMMENT
+        // let timelineControl = L.timelineSliderControl({
+        //     formatOutput: function (date) {
+        //         return new Date(date).toDateString();
+        //     }
+        // });
 
-        timelineControl.addTo(map1);
+        // timelineControl.addTo(map1);
+        // MOVED ABOVE CODE TO MAP1.JS FILE - DO NOT UNCOMMENT
+
         timelineControl.addTimelines(timeline);
         timeline.addTo(map1);
 
-        // timeline.on('change', function () {
-        //     updateList(timeline);
-        // });
     });
--- code/template/static/map1_pore_pressure_layer.js
+++ code/template/static/map1_pore_pressure_layer.js
@@ -5,14 +5,44 @@ fetch('/data')
         // Process data received from the server
         console.log(data);
 
+        let getInterval = function (feature) {
+            let startTime = new Date(feature.properties["Time"]).getTime();
+            let endTime = new Date(feature.properties["Time"]).getTime();
+            return { start: startTime, end: endTime };
+        };
+
         let timeline_pore_pressure = L.timeline(data, {
             getInterval: getInterval,
             pointToLayer: function (feature, latlng) {
-                let pressure = feature.properties["INSERT HERE"];
-                let radius = Math.max(3, Math.sqrt(volume) / 20); 
-                return L.circleMarker(latlng, { radius: radius }).bindPopup(`Volume Injected: ${volume} BBLs`);
+                let pressure = feature.properties["Pressure"];
+                // let radius = Math.max(3, Math.sqrt(volume) / 20); 
+                // return L.circleMarker(latlng, { radius: radius }).bindPopup(`Volume Injected: ${volume} BBLs`);
             }
         });
+
+        timelineControl.addTimelines(timeline_pore_pressure);
+        timeline_pore_pressure.addTo(map1);
+
+        //--------------------------------------------------
+        
+        var data = [
+            { lat: 31.53370266496937, lon: -103.777418085535, value: 10 },
+            { lat: 31.534198092955776, lon: -103.760445906687, value: 20 },
+            // Add more data points as needed
+        ];
+
+        // Create hexbin layer
+        let hexLayer = L.hexbinLayer({
+            radius: 20,  // Adjust this value as needed
+            opacity: 0.7,
+            colorScaleExtent: [0, 100],  // Define the range of your numerical values
+            colorRange: ['blue', 'red']  // Define the color gradient
+        }).addTo(map1);
+
+        // Add data to the hexbin layer
+        hexLayer.data(data.map(function(d) {
+            return [d.lat, d.lon, d.value];
+        }));
 
     })
     .catch(error => {
@@ -20,41 +50,41 @@ fetch('/data')
     });
 
 // --------------------------------------------------------------------------------------------------------------------------
-// Roxana's code for reference......
+// Roxana's injection code for reference......
 // --------------------------------------------------------------------------------------------------------------------------
 
-fetch('.../data/injection_data_v2.geojson')
-    .then(response => response.json())
-    .then(data => {
-        let getInterval = function (feature) {
-            let startTime = new Date(feature.properties["Injection Date"]).getTime();
-            let endTime = new Date(feature.properties["Injection End Date"]).getTime();
-            return { start: startTime, end: endTime };
-        };
+// fetch('../../data/injection_data_v2.geojson')
+//     .then(response => response.json())
+//     .then(data => {
+//         let getInterval = function (feature) {
+//             let startTime = new Date(feature.properties["Injection Date"]).getTime();
+//             let endTime = new Date(feature.properties["Injection End Date"]).getTime();
+//             return { start: startTime, end: endTime };
+//         };
 
-        let timeline = L.timeline(data, {
-            getInterval: getInterval,
-            pointToLayer: function (feature, latlng) {
-                let volume = feature.properties["Volume Injected (BBLs)"];
-                let radius = Math.max(3, Math.sqrt(volume) / 20); 
-                return L.circleMarker(latlng, { radius: radius }).bindPopup(`Volume Injected: ${volume} BBLs`);
-            }
-        });
+//         let timeline = L.timeline(data, {
+//             getInterval: getInterval,
+//             pointToLayer: function (feature, latlng) {
+//                 let volume = feature.properties["Volume Injected (BBLs)"];
+//                 let radius = Math.max(3, Math.sqrt(volume) / 20); 
+//                 return L.circleMarker(latlng, { radius: radius }).bindPopup(`Volume Injected: ${volume} BBLs`);
+//             }
+//         });
 
-        let timelineControl = L.timelineSliderControl({
-            formatOutput: function (date) {
-                return new Date(date).toDateString();
-            }
-        });
+//         // let timelineControl = L.timelineSliderControl({
+//         //     formatOutput: function (date) {
+//         //         return new Date(date).toDateString();
+//         //     }
+//         // });
 
-        timelineControl.addTo(map1);
-        timelineControl.addTimelines(timeline);
-        timeline.addTo(map1);
+//         // timelineControl.addTo(map1);
+//         timelineControl.addTimelines(timeline);
+//         timeline.addTo(map1);
 
-        // timeline.on('change', function () {
-        //     updateList(timeline);
-        // });
-    });
+//         // timeline.on('change', function () {
+//         //     updateList(timeline);
+//         // });
+//     });
 
 // --------------------------------------------------------------------------------------------------------------------------
 // Code from 'earthquakes.html' file for reference.......
